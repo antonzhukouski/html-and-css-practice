e45a02beae2a8a9698d070fa41375e6ce087a6ad
Zhukouski Anton <pisikota@gmail.com>
2020-12-04 17:46:37 +0300

    cache deleted

--- Minsk.rb/Gatsby/.cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('../node_modules/gatsby-plugin-web-font-loader/gatsby-browser.js'),
-      options: {"plugins":[],"custom":{"families":["roboto","space-mono"],"urls":["/fonts/fonts.scss"]}},
-    },{
-      plugin: require('../gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
