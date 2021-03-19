ba4d6ba20120b10985bbbdce1ed42f2cb41f13e6
Zhukouski Anton <pisikota@gmail.com>
2021-03-19 14:02:08 +0300

    JSON data modified, acces to internal data denied

--- Minsk.rb/Gatsby/src/components/Events.js
+++ Minsk.rb/Gatsby/src/components/Events.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import {StaticQuery} from 'gatsby'
+import {StaticQuery, useStaticQuery, graphql} from 'gatsby'
 import Announce from './Announce'
 import mapPin from '../images/map-pin.svg'
 
@@ -9,25 +9,25 @@ class Event extends Component {
   }
   render() {
     return (
-        <div className='Event'>
-          <div className = 'date'>
-            {this.props.eventDate}
+      <div className='Event'>
+        <div className = 'date'>
+          {this.props.eventDate}
+        </div>
+        <div className = 'eventName'>
+          {this.props.eventName}
+        </div>
+        <div className = 'place'>
+          <div className = 'place__link'>
+            <img src = {mapPin} alt =''/>
+            <a href = {this.props.eventPlaceUrl}> 
+              {this.props.eventPlaceUrl}
+            </a>
           </div>
-          <div className = 'eventName'>
-            {this.props.eventName}
+          <div className = 'place__address'>
+            {this.props.eventPlace}
           </div>
-          <div className = 'place'>
-            <div className = 'place__link'>
-              <img src = {mapPin} alt =''/>
-              <a href = {this.props.eventPlaceUrl}> 
-                {this.props.eventPlaceUrl}
-              </a>
-            </div>
-            <div className = 'place__address'>
-              {this.props.eventPlace}
-            </div>
-          </div>
-          <Announce />
+        </div>
+        <Announce />
           <div className = 'about-event'>
             <div className = 'about-event__heading'>
               {this.props.aboutEventName}
@@ -36,40 +36,22 @@ class Event extends Component {
               {this.props.aboutEvent}
             </div>
           </div>
-        </div>
-      
+        </div>    
     )
   }
-}
-
-function getEventsList(data) {
-  const EventsArray = []
-  data.allEventsListJson.edges.forEach(item =>
-    EventsArray.push(
-      <Event
-        eventDate = {item.node.eventDate}
-        eventName = {item.node.eventName}
-        eventPlaceUrl = {item.node.eventPlaceUrl}
-        eventPlace = {item.node.eventPlace}
-        aboutEventName = {item.node.aboutEventName}
-        aboutEvent = {item.node.aboutEvent}
-        key = {item.node.eventDate}
-      />
-    )
-  )
-  return EventsArray
 }
 
 const Events = () => (
   <StaticQuery
     query={graphql`
-      { allEventsListJson {
+      {
+        allEventListJson {
           edges {
             node {
               eventName
               eventDate
-              eventPlace
               eventPlaceUrl
+              eventPlace
               aboutEventName
               aboutEvent
             }
@@ -81,4 +63,103 @@ const Events = () => (
   ></StaticQuery>
 )
 
+function getEventsList(data) {
+    const EventsArray = []
+    data.allEventListJson.edges.forEach(item =>
+      EventsArray.push(
+        <Event
+          eventDate = {item.node.eventDate}
+          eventName = {item.node.eventName}
+          eventPlaceUrl = {item.node.eventPlaceUrl}
+          eventPlace = {item.node.eventPlace}
+          aboutEventName = {item.node.aboutEventName}
+          aboutEvent = {item.node.aboutEvent}
+          key = {item.node.eventName}
+        />
+      )
+    )
+    return EventsArray
+  }
+
 export default Events
+
+
+// class Event extends Component {
+//   constructor(props) {
+//     super()
+//   }
+//   render() {
+//     return (
+//         <div className='Event'>
+//           <div className = 'date'>
+//             {this.props.eventDate}
+//           </div>
+//           <div className = 'eventName'>
+//             {this.props.eventName}
+//           </div>
+//           <div className = 'place'>
+//             <div className = 'place__link'>
+//               <img src = {mapPin} alt =''/>
+//               <a href = {this.props.eventPlaceUrl}> 
+//                 {this.props.eventPlaceUrl}
+//               </a>
+//             </div>
+//             <div className = 'place__address'>
+//               {this.props.eventPlace}
+//             </div>
+//           </div>
+//           <Announce />
+//           <div className = 'about-event'>
+//             <div className = 'about-event__heading'>
+//               {this.props.aboutEventName}
+//             </div>
+//             <div className = 'about-event__text'>
+//               {this.props.aboutEvent}
+//             </div>
+//           </div>
+//         </div>
+      
+//     )
+//   }
+// }
+
+// function getEventsList(data) {
+//   const EventsArray = []
+//   data.allEventsListJson.edges.forEach(item =>
+//     EventsArray.push(
+//       <Event
+//         eventDate = {item.node.eventDate}
+//         eventName = {item.node.eventName}
+//         eventPlaceUrl = {item.node.eventPlaceUrl}
+//         eventPlace = {item.node.eventPlace}
+//         aboutEventName = {item.node.aboutEventName}
+//         aboutEvent = {item.node.aboutEvent}
+//         key = {item.node.eventDate}
+//       />
+//     )
+//   )
+//   return EventsArray
+// }
+
+// const Events = () => (
+//   <StaticQuery
+//     query={graphql`
+//       { allEventsListJson {
+//           edges {
+//             node {
+//               eventName
+//               eventDate
+//               eventPlace
+//               eventPlaceUrl
+//               aboutEventName
+//               aboutEvent
+//             }
+//           }
+//         }
+//       }
+//     `}
+//     render={data => getEventsList(data)}
+//   ></StaticQuery>
+// )
+
+// export default Events
--- Minsk.rb/Gatsby/src/components/Schedule.js
+++ Minsk.rb/Gatsby/src/components/Schedule.js
@@ -1,60 +1,102 @@
 import React, {Component} from 'react'
-import {StaticQuery} from 'gatsby'
+import {useStaticQuery, graphql} from 'gatsby'
 
-class ScheduleStructure extends Component {
-  constructor(props) {
-    super()
-  }
-  render() {
-    return (
-        <div className = 'schedule' >
-          <div className = 'schedule__block'>
-            <div className = 'schedule__block-time'>
-              {this.props.time}
-            </div>
-            <div className = 'schedule__block-event'>
-              {this.props.theme}
-            </div>
-            <div className = 'schedule__block-speaker'>
-              {this.props.speaker}
-            </div>
-          </div>
-        </div>
-      
-    )
-  }
-}
-
-function getSchedule(data) {
-  const scheduleArray = []
-  data.allBeerUpScheduleJson.edges.forEach(item =>
-    scheduleArray.push(
-      <ScheduleStructure
-        time = {item.node.time}
-        theme = {item.node.theme}
-        speaker = {item.node.speaker}
-      />
-    )
-  )
-  return scheduleArray
-}
-
-const Schedule = () => (
-  <StaticQuery
-    query={graphql`
-      { allBeerUpScheduleJson {
-          edges {
-            node {
+export default function Schedule() {
+  const data = useStaticQuery(graphql`
+    query Schedule {
+      allEventListJson {
+        edges {
+          node {
+            schedule {
               time
-              speaker
               theme
+              speaker
+              speakerPhoto
             }
           }
         }
       }
-    `}
-    render={data => getSchedule(data)}
-  ></StaticQuery>
-)
+    }
+  `)
 
-export default Schedule
+  const schedule = data.allEventListJson.edges.node.schedule.map (({node}) => {
+    const {time, theme, speaker, speakerPhoto} = node;
+    return {
+      time,
+      theme,
+      speaker,
+      speakerPhoto
+    }
+  })
+
+  return (
+    <div>
+      {schedule.map (({time, theme, speaker, speakerPhoto}) => 
+        <div className = 'tesDiv' key = 'time'> 
+          {time}
+          {theme}
+          {speaker}
+          {speakerPhoto}
+        </div>
+        )}
+    </div>
+  )
+}
+
+// class ScheduleStructure extends Component {
+//   constructor(props) {
+//     super()
+//   }
+//   render() {
+//     return (
+//         <div className = 'schedule' >
+//           <div className = 'schedule__block'>
+//             <div className = 'schedule__block-time'>
+//               {this.props.time}
+//             </div>
+//             <div className = 'schedule__block-event'>
+//               {this.props.theme}
+//             </div>
+//             <div className = 'schedule__block-speaker'>
+//               {this.props.speaker}
+//             </div>
+//           </div>
+//         </div>
+      
+//     )
+//   }
+// }
+
+// function getSchedule(data) {
+//   const scheduleArray = []
+//   data.allBeerUpScheduleJson.edges.forEach(item =>
+//     scheduleArray.push(
+//       <ScheduleStructure
+//         time = {item.node.time}
+//         theme = {item.node.theme}
+//         speaker = {item.node.speaker}
+//       />
+//     )
+//   )
+//   return scheduleArray
+// }
+
+// const Schedule = () => (
+//   <StaticQuery
+//     query={graphql`
+//       { allBeerUpScheduleJson {
+//           edges {
+//             node {
+//               time
+//               speaker
+//               theme
+//             }
+//           }
+//         }
+//       }
+//     `}
+//     render={data => getSchedule(data)}
+//   ></StaticQuery>
+// )
+
+// export default Schedule
